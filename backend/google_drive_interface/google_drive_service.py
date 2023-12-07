ace6947bd514080be96b3b04f9e07ec762913c09
David Liu <david_liu@tamu.edu>
2023-12-07 00:31:15 -0600

    minor type change in google_drive_service.py

--- backend/google_drive_interface/google_drive_service.py
+++ backend/google_drive_interface/google_drive_service.py
@@ -8,7 +8,7 @@ class GoogleDriveService:
         self._SCOPES=['https://www.googleapis.com/auth/drive']
 
     def build(self):
-        creds = ServiceAccountCredentials.from_json_keyfile_name(os.getenv("GOOGLE_APPLICATION_CREDENTIALS"), self._SCOPES)
+        creds = ServiceAccountCredentials.from_json_keyfile_name(os.getenv("GOOGLE_APPLICATION_CREDENTIALS"), self._SCOPES[0])
         service = build('drive', 'v3', credentials=creds)
 
         return service
