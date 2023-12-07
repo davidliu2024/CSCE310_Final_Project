199a9e722953a31574e23d4b141bf9a4d5a0db79
Hayden Holt <hholt@client-10-229-254-158.tamulink.tamu.edu>
2023-12-07 13:18:35 -0600

    finished course enrollment hooks

--- backend/toolkit/class_enrollment_tools.py
+++ backend/toolkit/class_enrollment_tools.py
@@ -18,5 +18,39 @@ def create_enrollment( enrollmentJSON ) -> ClassEnrollment:
 
     return enrollment
 
+
+def patch_enrollment ( enrollmentJSON) -> ClassEnrollment:
+
+
+    enrollment = ClassEnrollment(
+        ce_num = enrollmentJSON.get('ce_num'),
+        uin = enrollmentJSON.get('uin'),
+        class_id = enrollmentJSON.get('class_id'),
+        class_status = enrollmentJSON.get('class_status'),
+        semester = enrollmentJSON.get('semester'),
+        class_year = enrollmentJSON.get('class_year'),
+    )
+
+    enrollment.update()
+
+
+    return enrollment
+
+def delete_enrollment ( enrollmentJSON ) -> ClassEnrollment:
+
+
+    enrollment = ClassEnrollment(
+        ce_num = enrollmentJSON.get('ce_num'),
+    )
+
+    enrollment.delete()
+
+
+    return enrollment
+
+
+
+
+    
 #TODO: FINISH THE REST OF THIS. -Hayden
 
