cd948f9929c71658ade12ba3e9e246081a9387d7
Hayden Holt <hholt@client-10-229-254-158.tamulink.tamu.edu>
2023-12-08 16:12:30 -0600

    user document upload

--- frontend/app/student/documents/page.js
+++ frontend/app/student/documents/page.js
@@ -1,7 +1,7 @@
 'use client'
 
 import { useUserStore } from "@/store/store"
-import { useEffect, useState } from "react"
+import { useEffect, useState, useMemo } from "react"
 import * as EmailValidator from 'email-validator';
 
 import { AgGridReact } from 'ag-grid-react'; // React Grid Logic
@@ -9,53 +9,57 @@ import { AgGridReact } from 'ag-grid-react'; // React Grid Logic
 import "ag-grid-community/styles/ag-grid.css"; // Core CSS
 import "ag-grid-community/styles/ag-theme-quartz.css"; // Theme
 
-const deleteButton = (props) => {
-  const deleteReq = async () => {
-    const response = await fetch(`https://csce-310-flask-backend-api.onrender.com/users/${props.data.uin}`, {
-      method: 'DELETE',
-      headers: {
-        'Authorization': 'Basic ' + Buffer.from(username + ":" + password).toString('base64')
-      }
-    })
 
-    console.log(response)
-    // const code = response.status
-
-    // if (code === 200) {
-      // delete row from the table
-    // }
-    // else {
-
-    // }
-
-  }
-
-  return (
-    <button
-      onClick={deleteReq}
-      className='bg-red-500 hover:bg-red-400 text-white font-bold px-4 rounded h-10'
-    > Delete
-    </button>
-  )
-}
 
 export default function Home() {
 
   // data
   const globalState = useUserStore()
-  const [data, setData] = useState([])
-  const [tempData, setTempData] = useState([])
+  const [allDocuments, setAllDocuments] = useState([])
+  const [editing, setEditing] = useState(false)
+  const [file, setfile] = useState()
 
   // logistics state
-  const [editing, setEditing] = useState(false)
+
   const [statusCode, setStatusCode] = useState('')
-  const [areEmailsValid, setAreEmailsValid] = useState(true)
+
 
   // get initial data
+
+  const deleteButton = (props) => {
+    const deleteReq = async () => {
+      console.log(props.route.doc_id)
+      const response = await fetch(`https://csce-310-flask-backend-api.onrender.com/${props.route.doc_id}/document}`, {
+        method: 'DELETE',
+        headers: {
+          'Authorization': 'Basic ' + Buffer.from(globalState.username + ":" + globalState.password).toString('base64')
+        }
+      })
+  
+      const code = response.status
+      setStatusCode(code)
+
+      if (code === 200) {
+        const json = await response.json()
+        console.log(json)
+
+        setAllDocuments(json)
+      }
+  
+    }
+  
+    return (
+      <button
+        onClick={deleteReq}
+        className='bg-red-500 hover:bg-red-400 text-white font-bold px-4 rounded h-10'
+      > Delete
+      </button>
+    )
+  }
   useEffect(() => {
     const fetchUsers = async () => {
-      console.log('sup1')
-      const response = await fetch(`https://csce-310-flask-backend-api.onrender.com/users`, {
+   
+      const response = await fetch(`https://csce-310-flask-backend-api.onrender.com/applications/documents/user`, {
         method: 'GET',
         headers: {
           'Authorization': 'Basic ' + Buffer.from(globalState.username + ":" + globalState.password).toString('base64')
@@ -69,167 +73,100 @@ export default function Home() {
         const json = await response.json()
         console.log(json)
 
-        setData(json)
+        setAllDocuments(json)
       }
 
     }
 
     fetchUsers()
-      .catch(err => console.log(err, 'from the admin/users page'))
+      .catch(err => console.log(err, 'from the student/documents page'))
   }, [])
 
-  // validate if data is correct
-  const validateEmails = () => {
-    if (data && data.some(e => !EmailValidator.validate(e.email))) {
-      setAreEmailsValid(false)
+
+  const saveData = async () => {
+
+
+
+    if (file) {
+      try {
+        const fileData = new FormData()
+        fileData.set('file', file)
+       
+        console.log(fileData)
+        
+        const res = await fetch(`https://csce-310-flask-backend-api.onrender.com/applications/documents/4`, {
+          method: 'POST',
+          headers: {
+            'Authorization': 'Basic ' + Buffer.from(globalState.username + ":" + globalState.password).toString('base64'),
+          },
+          body: fileData
+        })
+       
+     
+        if (!res.ok) throw new Error(await res.json())
+      } catch (e) {
+        console.error(e)
+      }
+
     }
-    else {
-      setAreEmailsValid(true)
-    }
+
+ 
   }
 
-  const startEditing = () => {
-    setTempData(data)
-    setEditing(true)
-  }
-
-  // helpers
-  const addNewUser = () => {
-    startEditing()
-
-    setData([...data, {
-      "uin": Math.max(...data.map(e => e.uin)) + 1,
-      "first_name": '',
-      'last_name': '',
-      'm_initial': '',
-      'password': '',
-      "discord_name": '',
-      "user_type": '',
-      "username": '',
-      "email": '',
-    }])
-  }
-
-  // TODO: save the data
-  const saveData = () => {
-    // TODO: create new users
-
-    // TODO: update admin status
-
-    // TODO: update user's details
-  }
-
-  // const cancelChanges = () => {
-  //   setEditing(false)
-  //   setData([...tempData])
-  // }
 
   return (
     <main className="h-screen flex flex-col px-5 py-3 gap-5">
-      <h1 class="text-3xl">Manage Users</h1>
+      <h1 class="text-3xl">Documents</h1>
 
+      
       <div className="flex gap-3">
-        <button
-          onClick={addNewUser}
-          className='bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded w-42'
-        >Add new user (+)</button>
 
-        {
-          (editing) ? '' :
-            <button
-              onClick={startEditing}
-              className='bg-purple-500 hover:bg-purple-400 text-white font-bold py-2 px-4 border-b-4 border-purple-700 hover:border-purple-500 rounded w-42'
-            >Edit Table (+)</button>
-        }
+            <form >
+              <input
+                type="file"
+                name="file"
+                onChange={(e) => setfile(e.target.files?.[0])}//
+                
+              />
+            </form>
 
-        {
-          (!editing) ? '' :
+
             <button
               onClick={saveData}
               className='bg-green-500 hover:bg-green-400 text-white font-bold py-2 px-4 border-b-4 border-green-700 hover:border-green-500 rounded w-42'
-            >Save Changes</button>
-        }
+            >Submit Document</button>
+        
 
-        {/* <button TODO:
-          onClick={cancelChanges}
-          className='bg-red-500 hover:bg-red-400 text-white font-bold py-2 px-4 border-b-4 border-red-700 hover:border-red-500 rounded w-42'
-        >Cancel Changes</button> */}
-
-        {!areEmailsValid ?
-          <span className="text-sm text-red-600">Ensure no emails are empty & all are valid</span> : ''
-        }
       </div>
+      <div className="ag-theme-quartz h-screen">
+         <AgGridReact
 
-      {/* <div className="ag-theme-quartz h-screen"> */}
-        {/* <AgGridReact
-          onCellKeyDown={validateEmails}
-          // rowData={data}
+          rowData={allDocuments}
           columnDefs={[
             {
-              "field": "uin",
-              "headerName": "UIN"
+              "field": "program_name",
+              "headerName": "App Number",
+              
             },
             {
-              "field": "username",
-              "headerName": "Username"
-            },
-            {
-              "field": "email",
-              "headerName": "Email",
-              "editable": (params) => editing && !!!params.data.username,
-              "cellStyle": (params) => ({ "backgroundColor": (editing && !!!params.data.username) ? '#FFFFE0' : '' })
-            },
-            {
-              "field": 'first_name',
-              "editable": editing,
-              "headerName": "First Name",
-              "cellStyle": { "backgroundColor": (editing) ? '#FFFFE0' : '' }
-            },
-            {
-              "field": 'last_name',
-              "editable": editing,
-              "headerName": "Last Name",
-              "cellStyle": { "backgroundColor": (editing) ? '#FFFFE0' : '' }
-            },
-            {
-              "field": 'm_initial',
-              "editable": editing,
-              "headerName": "Middle Initial",
-              "cellStyle": { "backgroundColor": (editing) ? '#FFFFE0' : '' }
-            },
-            {
-              "field": 'password',
-              "editable": editing,
-              "headerName": "Password",
-              "cellStyle": { "backgroundColor": (editing) ? '#FFFFE0' : '' }
-            },
-            {
-              "field": "discord_name",
-              "editable": editing,
-              "headerName": "Discord Name",
-              "cellStyle": { "backgroundColor": (editing) ? '#FFFFE0' : '' }
-            },
-            {
-              "field": "user_type",
-              "editable": editing,
-              "headerName": "User Type",
-              "cellStyle": { "backgroundColor": (editing) ? '#FFFFE0' : '' },
-              "cellEditor": "agSelectCellEditor",
-              "cellEditorParams": {
-                "values": ["ADMIN", "USER"]
-              }
-            },
-            {
-              "headerName": "Deactivate",
-              // "cellRenderer": <button
+              "field": "doc_link",
+              "headerName": "Link",
+              "editable":true
             },
             {
               "headerName": "Delete",
-              "cellRenderer": deleteButton
+              "cellRenderer": deleteButton,
+              "cellRendereParams": {
+                "doc_id": 12
+              }
             }
+           
           ]}
-        />
-      </div> */}
+          
+        /> 
+      </div>
+     
+      
     </main>
   )
 }
